0f54666827fb3b71187e45abcb681c4c561ab80e
Hector Polanco <hdp2112@columbia.edu>
2019-10-24 04:54:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,14 +3,6 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |subkey, name|
       name.each do |name|
-        if !new_data[name]
-          new_data[name] = {}
-        end
-        
-        if !new_data[name][key]
-          new_data[name][key] = []
-        end
-        
         new_data[name][key] << subkey.to_s
       end
     end
